26e21ea07d2ca37ce1cc55f8af640ce7f1ca4b66
Cédric Salvador <csalvador@gpcsolutions.fr>
2012-07-03 18:48:27 +0200

    the llx_product_stock_det table is automatically created when the module is enabled

---
+++ htdocs/detailedstock/sql/llx_product_stock_det.sql
@@ -0,0 +1,12 @@
+CREATE TABLE IF NOT EXISTS llx_product_stock_det (
+  rowid integer NOT NULL auto_increment primary key ,
+  tms_i TIMESTAMP NOT NULL ,
+  tms_o TIMESTAMP NULL ,
+  fk_product integer NOT NULL ,
+  fk_entrepot integer NOT NULL ,
+  fk_user_author_i integer NULL ,
+  fk_user_author_o integer NULL ,
+  serial VARCHAR(128) NULL ,
+  serial_type integer NULL ,
+  price DOUBLE NULL )
+ENGINE = innodb;
