26957e770a793c8819b6442e3a1156bba76bffd1
Raphaël Doursenaud <rdoursenaud@gpcsolutions.fr>
2012-07-06 16:23:04 +0200

    Formatted source according to accepted coding standards

--- htdocs/detailedstock/sql/llx_product_stock_det.sql
+++ htdocs/detailedstock/sql/llx_product_stock_det.sql
@@ -14,14 +14,14 @@
 -- You should have received a copy of the GNU General Public License
 -- along with this program.  If not, see <http://www.gnu.org/licenses/>.
 CREATE TABLE IF NOT EXISTS llx_product_stock_det (
-  rowid integer NOT NULL auto_increment primary key ,
-  tms_i TIMESTAMP NOT NULL ,
-  tms_o TIMESTAMP NULL ,
-  fk_product integer NOT NULL ,
-  fk_entrepot integer NOT NULL ,
-  fk_user_author_i integer NULL ,
-  fk_user_author_o integer NULL ,
-  serial VARCHAR(128) NULL ,
-  serial_type integer NULL ,
-  price DOUBLE NULL )
+    rowid integer NOT NULL auto_increment primary key ,
+    tms_i TIMESTAMP NOT NULL ,
+    tms_o TIMESTAMP NULL ,
+    fk_product integer NOT NULL ,
+    fk_entrepot integer NOT NULL ,
+    fk_user_author_i integer NULL ,
+    fk_user_author_o integer NULL ,
+    serial VARCHAR(128) NULL ,
+    serial_type integer NULL ,
+    price DOUBLE NULL )
 ENGINE = innodb;
