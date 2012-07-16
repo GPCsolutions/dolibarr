3c33402f22ef31e3aae6a8be7910e710dfcaf3ef
Cédric Salvador <csalvador@gpcsolutions.fr>
2012-07-16 12:07:49 +0200

    scrapped the trigger

--- htdocs/detailedstock/core/modules/modDetailedStock.class.php
+++ htdocs/detailedstock/core/modules/modDetailedStock.class.php
@@ -82,9 +82,7 @@ class modDetailedStock extends DolibarrModules
         //      'hooks' => array('hookcontext1','hookcontext2')                             // Set here all hooks context managed by module
         //      'workflow' => array('order' => array('WORKFLOW_ORDER_AUTOCREATE_INVOICE'))  // Set here all workflow context managed by module
         //                        );
-        $this->module_parts = array(
-            'triggers' => 1
-        );
+        $this->module_parts = array();
 
         // Data directories to create when module is enabled.
         // Example: this->dirs = array("/detailedStock/temp");
