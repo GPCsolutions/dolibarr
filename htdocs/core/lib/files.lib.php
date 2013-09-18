cb0febbda95228ea9ddaf940e88e473fee887cdd
Cédric Salvador <csalvador@gpcsolutions.fr>
2013-09-18 12:13:50 +0200

    checkstyle

--- htdocs/core/lib/files.lib.php
+++ htdocs/core/lib/files.lib.php
@@ -994,6 +994,7 @@ function dol_init_file_process($pathtoscan='')
  * @param	int		$allowoverwrite			1=Allow overwrite existing file
  * @param	int		$donotupdatesession		1=Do no edit _SESSION variable
  * @param	string	$varfiles				_FILES var name
+ * @param	string	$link					Link to add
  * @return	void
  */
 function dol_add_file_process($upload_dir,$allowoverwrite=0,$donotupdatesession=0,$varfiles='addedfile', $link=null)
