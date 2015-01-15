edaa336154b3c2dbb0c3f0a2b63d56ce69fc6983
Marcos García de La Fuente <marcosgdf@gmail.com>
2015-01-15 13:05:32 +0100

    Added current view for tasks

--- htdocs/core/menus/standard/eldy.lib.php
+++ htdocs/core/menus/standard/eldy.lib.php
@@ -1099,16 +1099,26 @@ function print_left_eldy_menu($db,$menu_array_before,$menu_array_after,&$tabMenu
 				}
 
 				// Project affected to user
-				$newmenu->add("/projet/activity/index.php?mode=mine", $langs->trans("MyActivities"), 0, $user->rights->projet->lire);
-				$newmenu->add("/projet/tasks.php?action=create&mode=mine", $langs->trans("NewTask"), 1, $user->rights->projet->creer);
-				$newmenu->add("/projet/tasks/index.php?mode=mine", $langs->trans("List"), 1, $user->rights->projet->lire);
-				$newmenu->add("/projet/activity/list.php?mode=mine", $langs->trans("NewTimeSpent"), 1, $user->rights->projet->creer);
+				$newmenu->add("/projet/activity/index.php?mode=mine&leftmenu=mytasks", $langs->trans("MyActivities"), 0, $user->rights->projet->lire, '', $mainmenu, 'mytasks');
+				$newmenu->add("/projet/tasks.php?action=create&mode=mine&leftmenu=mytasks", $langs->trans("NewTask"), 1, $user->rights->projet->creer);
+				$newmenu->add("/projet/tasks/index.php?mode=mine&leftmenu=mytasks", $langs->trans("List"), 1, $user->rights->projet->lire);
+
+				if (empty($leftmenu) || $leftmenu == 'mytasks') {
+					$newmenu->add("/projet/tasks/current.php?leftmenu=mytasks&mode=mine", $langs->trans("Current"), 2, $user->rights->projet->lire);
+				}
+
+				$newmenu->add("/projet/activity/list.php?mode=mine&leftmenu=mytasks", $langs->trans("NewTimeSpent"), 1, $user->rights->projet->creer);
 
 				// All project i have permission on
-				$newmenu->add("/projet/activity/index.php", $langs->trans("Activities"), 0, $user->rights->projet->lire && $user->rights->projet->lire);
-				$newmenu->add("/projet/tasks.php?action=create", $langs->trans("NewTask"), 1, $user->rights->projet->creer && $user->rights->projet->creer);
-				$newmenu->add("/projet/tasks/index.php", $langs->trans("List"), 1, $user->rights->projet->lire && $user->rights->projet->lire);
-				$newmenu->add("/projet/activity/list.php", $langs->trans("NewTimeSpent"), 1, $user->rights->projet->creer && $user->rights->projet->creer);
+				$newmenu->add("/projet/activity/index.php?leftmenu=tasks", $langs->trans("Activities"), 0, $user->rights->projet->lire, '', $mainmenu, 'tasks');
+				$newmenu->add("/projet/tasks.php?action=create&leftmenu=tasks", $langs->trans("NewTask"), 1, $user->rights->projet->creer);
+				$newmenu->add("/projet/tasks/index.php?leftmenu=tasks", $langs->trans("List"), 1, $user->rights->projet->lire);
+
+				if (empty($leftmenu) || $leftmenu == 'tasks') {
+					$newmenu->add("/projet/tasks/current.php?leftmenu=tasks&mode=mine", $langs->trans("Current"), 2, $user->rights->projet->lire);
+				}
+
+				$newmenu->add("/projet/activity/list.php?leftmenu=tasks", $langs->trans("NewTimeSpent"), 1, $user->rights->projet->creer);
 			}
 		}
 
