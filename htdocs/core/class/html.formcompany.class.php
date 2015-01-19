9a87044b8868364154a5979cb6cc596b10972ce1
Marcos García de La Fuente <marcosgdf@gmail.com>
2015-01-19 12:37:03 +0100

    Little correction

--- htdocs/core/class/html.formcompany.class.php
+++ htdocs/core/class/html.formcompany.class.php
@@ -592,9 +592,10 @@ class FormCompany
 									function(response) {
 										if (response != null)
 										{
+											var num = response.num;
+
 											$.each(obj.params, function(key,action) {
 												if (key.length) {
-													var num = response.num;
 													if (num > 0) {
 														$("#" + key).removeAttr(action);
 													} else {
@@ -603,7 +604,10 @@ class FormCompany
 												}
 											});
 											$("select#" + htmlname).html(response.value);
+										} else {
+											var num = 0;
 										}
+
 										if (response.num) {
 
 											var selected = $("select#" + htmlname+ " option:first");
