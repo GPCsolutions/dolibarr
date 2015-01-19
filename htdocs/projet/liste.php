1eab723206a729e127737808c2cc4d21b0678f86
Marcos García de La Fuente <marcosgdf@gmail.com>
2015-01-19 12:05:46 +0100

    Added Copyright info

--- htdocs/projet/liste.php
+++ htdocs/projet/liste.php
@@ -4,6 +4,7 @@
  * Copyright (C) 2005      Marc Bariley / Ocebo <marc@ocebo.com>
  * Copyright (C) 2005-2010 Regis Houssin        <regis.houssin@capnetworks.com>
  * Copyright (C) 2013      Cédric Salvador      <csalvador@gpcsolutions.fr>
+ * Copyright (C) 2015      Marcos García        <marcosgdf@gmail.com>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
